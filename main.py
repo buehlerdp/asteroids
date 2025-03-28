ecdf601e9dd8a01a235ead984f6cd5993143d97e
buehlerdp <buehlerdp@gmail.com>
2025-03-28 13:08:16 -0500

    initial game looop

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 
+pygame.init()
+
 def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
@@ -10,3 +12,9 @@ def main():
 if __name__ == "__main__":
     main()
 
+while True:
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
+    pygame.surface.fill(0,0,0)
+    pygame.displacy.flip()
